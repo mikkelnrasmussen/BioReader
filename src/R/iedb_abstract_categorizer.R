4da6293dd5c1796d5c12f7abb8057388834891e2
mikkelnrasmussen <s193518@student.dtu.dk>
2023-10-05 23:07:51 +0200

    fix: readding all files

---
+++ src/R/iedb_abstract_categorizer.R
@@ -0,0 +1,197 @@
+## Call libraries
+library(tidyverse)
+library(httr)
+library(xml2)
+library(rentrez) # not on DTU server
+library(textrecipes) # not on DTU server
+library(tidymodels) # not on DTU server
+library(discrim) # not on DTU server
+library(plsmod) # not on DTU Heath Tech server
+library(plyr)
+library(baguette)
+library(rules)
+library(readxl)
+library(themis)
+
+# Load the data
+file_names <- dir("data/training_data", full.names = TRUE)
+df_all_classes <- do.call(rbind, lapply(file_names, read.csv))
+df_class_label <- read_excel("data/All_Updated_Categories_2019_edited_by_mikkel.xlsx")
+
+# QC: Check if all the cateogries are present in both the metadata file
+# and the data files
+df_all_classes_only <- df_all_classes |>
+  filter(!(SubType %in% df_class_label$subcategory)) |>
+  select(SubType) |>
+  pull() |>
+  unique()
+
+df_class_label_only <- df_class_label |>
+  filter(!(Abbreviation %in% df_all_classes$SubType)) |>
+  select(Abbreviation) |>
+  pull() |>
+  unique()
+
+# Perform inner join to only keep the categories that are in common
+df_merged <- df_all_classes |>
+  inner_join(
+    df_class_label,
+    by = c("SubType" = "Abbreviation")
+  )
+
+# QC: Check which columns contain NAs
+df_merged |>
+  is.na() |>
+  colSums()
+
+# Create dataframe with all the classes
+df_main_classes <- df_merged |>
+  select(PubMed_ID, Title, Abstract, Class) |>
+  dplyr::rename(pmid = PubMed_ID) |>
+  dplyr::rename_with(tolower)
+
+# QC: Check if there are any NAs
+df_main_classes |>
+  is.na() |>
+  colSums()
+
+# Create dataframe with all categories
+df_all_classes <- df_merged |>
+  select(PubMed_ID, Title, Abstract, SubType) |>
+  dplyr::rename(pmid = PubMed_ID) |>
+  dplyr::rename(class = SubType) |>
+  dplyr::rename_with(tolower) |>
+  as_tibble()
+
+# QC: Check if there are any NAs
+df_all_classes |>
+  is.na() |>
+  colSums()
+
+# Create training and test set
+set.seed(123)
+split <- initial_split(df_all_classes, strata = class, prop = 0.90)
+training_data <- training(split)
+testing_data <- testing(split)
+
+training_data <- training_data[, c("pmid", "abstract", "class")]
+train_rec <-
+  recipe(class ~ ., data = training_data) |>
+  update_role(pmid, new_role = "id") |>
+  step_tokenize(abstract) |>
+  step_stopwords(abstract) |>
+  step_stem(abstract) |>
+  step_tokenfilter(abstract, max_tokens = 500) |>
+  step_tfidf(abstract) |>
+  step_downsample(class)
+
+train_prep <- prep(train_rec)
+train_prep
+
+# We will save a data frame from the PREP to use later with another algo
+train_baked <- bake(train_prep, new_data = NULL)
+# write.csv(train_baked, "data/abstracts_baked.csv", row.names = FALSE)
+dim(train_baked)
+
+## Cross Validation Split of Training Data
+set.seed(888)
+train_folds <- vfold_cv(data = training_data, v = 10)
+train_folds
+
+# Lasso
+lasso_spec <- multinom_reg(
+  penalty = tune(),
+  mixture = 1
+) |>
+  set_mode("classification") |>
+  set_engine("nnet")
+
+# Support Vector Machine - polynomial degree = 1
+svmlinear_spec <- svm_poly(
+  # degree = 1,
+  # cost = tune()
+) |>
+  set_mode("classification") |>
+  set_engine("kernlab")
+
+# Support Vector Machine - radial basis function
+svmrbf_spec <- svm_rbf(
+  # cost = tune(),
+  # rbf_sigma = tune()
+) |>
+  set_mode("classification") |>
+  set_engine("kernlab")
+
+# Random Forest
+randomf_spec <- rand_forest(
+  # mtry = tune(),
+  # trees = tune(),
+  # min_n = tune()
+) |>
+  set_mode("classification") |>
+  set_engine("ranger")
+
+# XGBoost
+xgboost_spec <- boost_tree(
+  # trees = tune(),
+  # mtry = tune(),
+  # tree_depth = tune(),
+  # learn_rate = .01
+) |>
+  set_mode("classification") |>
+  set_engine("xgboost")
+
+# Neural network
+nnet_spec <- mlp(
+  # epochs = 30,
+  # hidden_units = tune(),
+  # dropout = tune()
+) |>
+  set_mode("classification") |>
+  set_engine("keras", verbose = 2)
+
+
+# Set up workflow set
+workflow_sets <- workflow_set(
+  preproc = list(train_rec),
+  models = list(
+    # lasso_spec,
+    svmrbf_spec,
+    xgboost_spec,
+    randomf_spec,
+    nnet_spec
+  ),
+  cross = TRUE
+)
+workflow_sets
+
+
+RUN <- TRUE
+if (RUN) {
+  doParallel::registerDoParallel()
+  start_time <- Sys.time()
+  start_time
+  fit_workflows <- workflow_sets |>
+    workflow_map(
+      seed = 888,
+      fn = "fit_resamples",
+      grid = 20,
+      resamples = train_folds,
+      verbose = TRUE
+    )
+  end_time <- Sys.time()
+  time_taken <- end_time - start_time
+  time_taken
+  doParallel::stopImplicitCluster()
+}
+fit_workflows
+if (RUN) {
+  saved_abstract_modelset <- fit_workflows
+  saveRDS(saved_abstract_modelset, "saved_abstract_modelset_all_data.rds")
+}
+
+if (!RUN) {
+  fit_workflows <- readRDS("saved_imdb_modelset.rds")
+}
+
+# autoplot(fit_workflows)
