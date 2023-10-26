65372722b16a126818f062cbf7d0a0f2079a1f4c
mikkelnrasmussen <s193518@student.dtu.dk>
2023-10-26 16:21:32 +0200

    fix: revert changes

--- src/bioreader_submit.sh
+++ src/bioreader_submit.sh
@@ -5,15 +5,13 @@
 ### -- set the job Name -- 
 #BSUB -J bioreader.R
 ### -- ask for 1 core -- 
-#BSUB -n 10
-### -- specify that the cores must be on the same host -- 
-#BSUB -R "span[hosts=1]"
-### -- specify that we need 20GB of memory per core/slot -- 
-#BSUB -R "rusage[mem=20GB]"
+#BSUB -n 5
+### -- specify that we need 2GB of memory per core/slot -- 
+#BSUB -R "rusage[mem=40GB]"
 ### -- specify that we want the job to get killed if it exceeds 3 GB per core/slot -- 
 #BSUB -M 50GB
 ### -- set walltime limit: hh:mm -- 
-#BSUB -W 12:00 
+#BSUB -W 24:00 
 ### -- set the email address -- 
 # please uncomment the following line and put in your e-mail address,
 # if you want to receive e-mail notifications on a non-default address
@@ -31,5 +29,4 @@ module load R/4.2.1-mkl2022update1
 #export TMPDIR=Path_to_your_scratch_directory
 export R_BATCH_OPTIONS="--no-save"
 # -- commands you want to execute -- # 
-R CMD BATCH minimal_example.R
-
+R CMD BATCH main_test.R
