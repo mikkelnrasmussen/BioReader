780cf8491bdffea7817935cd2c60b3035d55d563
mikkelnrasmussen <s193518@student.dtu.dk>
2023-10-28 11:35:34 +0200

    build: move parallel bioreader pbs summit script

--- parallel_bioreader_submit.sh
+++ parallel_bioreader_submit.sh
@@ -9,11 +9,11 @@
 ### -- specify that the cores must be on the same host -- 
 #BSUB -R "span[hosts=1]"
 ### -- specify that we need 2GB of memory per core/slot -- 
-#BSUB -R "rusage[mem=50GB]"
+#BSUB -R "rusage[mem=55GB]"
 ### -- specify that we want the job to get killed if it exceeds 3 GB per core/slot -- 
-#BSUB -M 60GB
+#BSUB -M 56GB
 ### -- set walltime limit: hh:mm -- 
-#BSUB -W 12:00 
+#BSUB -W 50:00 
 ### -- set the email address -- 
 # please uncomment the following line and put in your e-mail address,
 # if you want to receive e-mail notifications on a non-default address
@@ -27,10 +27,10 @@
 #BSUB -o bioreader_%J.out 
 #BSUB -e bioreader_%J.err
 
-module load R/4.2.1-mkl2022update1
+module load R/4.3.1-mkl2023update1
 
 #export TMPDIR=Path_to_your_scratch_directory
 export R_BATCH_OPTIONS="--no-save"
 mkdir -p time/
 # -- commands you want to execute -- # 
-/usr/bin/time -v -o time/bioreader.time Rscript R/minimal_test.R -t class --hpc
+/usr/bin/time -v -o time/bioreader.time Rscript src/R/minimal_example.R -a class --hpc
