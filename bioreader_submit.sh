87343540d253884b7bb6a12b4ce4339bed149125
mikkelnrasmussen <s193518@student.dtu.dk>
2023-10-28 11:35:17 +0200

    build: move bioreader pbs summit script

--- bioreader_submit.sh
+++ bioreader_submit.sh
@@ -3,15 +3,15 @@
 ### -- specify queue -- 
 #BSUB -q hpc
 ### -- set the job Name -- 
-#BSUB -J bioreader.R
+#BSUB -J bioreader
 ### -- ask for 1 core -- 
-#BSUB -n 5
+#BSUB -n 1
 ### -- specify that we need 2GB of memory per core/slot -- 
-#BSUB -R "rusage[mem=40GB]"
+#BSUB -R "rusage[mem=2GB]"
 ### -- specify that we want the job to get killed if it exceeds 3 GB per core/slot -- 
-#BSUB -M 50GB
+#BSUB -M 3GB
 ### -- set walltime limit: hh:mm -- 
-#BSUB -W 24:00 
+#BSUB -W 01:00 
 ### -- set the email address -- 
 # please uncomment the following line and put in your e-mail address,
 # if you want to receive e-mail notifications on a non-default address
@@ -25,8 +25,10 @@
 #BSUB -o Output_bioreader_%J.out 
 #BSUB -e Output_bioreader_%J.err
 
-module load R/4.2.1-mkl2022update1
+module load R/4.3.1-mkl2023update1
+
 #export TMPDIR=Path_to_your_scratch_directory
 export R_BATCH_OPTIONS="--no-save"
-# -- commands you want to execute -- # 
-R CMD BATCH main_test.R
+mkdir -p time/
+# -- commands you want to execute -- #
+/usr/bin/time -v -o time/bioreader.time Rscript src/R/minimal_example.R -a class --test
