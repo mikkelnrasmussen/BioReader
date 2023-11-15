6ca05ccd60c2d9a1e57bf4ced1706c3f4d3aa327
mikkelnrasmussen <s193518@student.dtu.dk>
2023-11-15 22:36:40 +0100

    analys: increase time usage

--- parallel_bioreader_submit.sh
+++ parallel_bioreader_submit.sh
@@ -13,7 +13,7 @@
 ### -- specify that we want the job to get killed if it exceeds 3 GB per core/slot -- 
 #BSUB -M 7GB
 ### -- set walltime limit: hh:mm -- 
-#BSUB -W 24:00 
+#BSUB -W 50:00 
 ### -- set the email address -- 
 # please uncomment the following line and put in your e-mail address,
 # if you want to receive e-mail notifications on a non-default address
