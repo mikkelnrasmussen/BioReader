da00a878ae3abf3a85c9347f2835295bc50828ec
mikkelnrasmussen <s193518@student.dtu.dk>
2023-11-15 21:24:25 +0100

    analys: train autoimmune model

--- parallel_bioreader_submit.sh
+++ parallel_bioreader_submit.sh
@@ -3,15 +3,15 @@
 ### -- specify queue -- 
 #BSUB -q hpc
 ### -- set the job Name -- 
-#BSUB -J bioreader_class
+#BSUB -J bioreader_autoimm
 ### -- ask for 1 core -- 
 #BSUB -n 32
 ### -- specify that the cores must be on the same host -- 
 #BSUB -R "span[hosts=1]"
 ### -- specify that we need 2GB of memory per core/slot -- 
-#BSUB -R "rusage[mem=18GB]"
+#BSUB -R "rusage[mem=15GB]"
 ### -- specify that we want the job to get killed if it exceeds 3 GB per core/slot -- 
-#BSUB -M 18.5GB
+#BSUB -M 15.5GB
 ### -- set walltime limit: hh:mm -- 
 #BSUB -W 50:00 
 ### -- set the email address -- 
@@ -33,4 +33,4 @@ module load R/4.3.1-mkl2023update1
 export R_BATCH_OPTIONS="--no-save"
 mkdir -p time/
 # -- commands you want to execute -- # 
-/usr/bin/time -v -o time/bioreader.time Rscript src/R/minimal_example.R -a class -p --hpc
+/usr/bin/time -v -o time/bioreader.time Rscript src/R/minimal_example.R -a Autoimm -p --hpc
