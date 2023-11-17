005c07c44f837ad10bfbd16eb817a12f52f0127a
mikkelnrasmussen <s193518@student.dtu.dk>
2023-11-17 12:07:42 +0100

    analys: train level 1 model with the HIV class

--- parallel_bioreader_submit.sh
+++ parallel_bioreader_submit.sh
@@ -3,17 +3,17 @@
 ### -- specify queue -- 
 #BSUB -q hpc
 ### -- set the job Name -- 
-#BSUB -J bioreader_allergen
+#BSUB -J bioreader_class_with_HIV
 ### -- ask for 1 core -- 
 #BSUB -n 32
 ### -- specify that the cores must be on the same host -- 
 #BSUB -R "span[hosts=1]"
 ### -- specify that we need 2GB of memory per core/slot -- 
-#BSUB -R "rusage[mem=7GB]"
+#BSUB -R "rusage[mem=17GB]"
 ### -- specify that we want the job to get killed if it exceeds 3 GB per core/slot -- 
-#BSUB -M 7.5GB
+#BSUB -M 17.5GB
 ### -- set walltime limit: hh:mm -- 
-#BSUB -W 50:00 
+#BSUB -W 55:00 
 ### -- set the email address -- 
 # please uncomment the following line and put in your e-mail address,
 # if you want to receive e-mail notifications on a non-default address
@@ -33,4 +33,4 @@ module load R/4.3.1-mkl2023update1
 export R_BATCH_OPTIONS="--no-save"
 mkdir -p time/
 # -- commands you want to execute -- # 
-/usr/bin/time -v -o time/bioreader.time Rscript src/R/main_PRIAT.R -a Allergen -p --hpc
+/usr/bin/time -v -o time/bioreader.time Rscript src/R/main_PRIAT.R -a class -p --hpc -d _with_HIV
