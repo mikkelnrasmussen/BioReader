9b3b4b9018453482b1d17758aac7c5b9f05e5c73
mikkelnrasmussen <s193518@student.dtu.dk>
2023-11-21 20:34:33 +0100

    analys: update evaluation of results

--- src/R/result_evaluation.R
+++ src/R/result_evaluation.R
@@ -3,19 +3,7 @@ library("tidymodels")
 library("here")
 
 ####################################################################
-############## Class classification - 1000 samples #################
-####################################################################
-
-# Read in overall stat reaults
-overall_stats_1 <- read_csv(here("results/rf_overall_stats_1000.csv"))
-View(overall_stats_1)
-
-# Read in by class reaults
-metrics_by_class_1 <- read_csv(here("results/rf_metric_by_class_1000.csv"))
-View(metrics_by_class_1)
-
-####################################################################
-############## Class classification - all data #####################
+################# Class classification - all data ##################
 ####################################################################
 
 # Read in overall stat reaults
@@ -34,43 +22,7 @@ collect_metrics(tune_results2) |>
   view()
 
 ####################################################################
-###### Class classification - 1000 samples - tune max_tokens #######
-####################################################################
-
-# Read in overall stat reaults
-overall_stats_3 <- read_csv(here("results/rf_overall_stats_class_1000.csv"))
-View(overall_stats_3)
-
-# Read in by class reaults
-metrics_by_class_3 <- read_csv(here("results/rf_metric_by_target_class_1000.csv"))
-View(metrics_by_class_3)
-
-tune_results3 <- read_rds("results/saved_abstract_modelset_minimal_example.rds")
-
-collect_metrics(tune_results3) |>
-  filter(.metric == "accuracy") |>
-  arrange(desc(mean)) |>
-  view()
-
-# Select the best model based on accuracy
-best_model <- tune_results3 |>
-  select_best("accuracy")
-
-# Create the final workflow with the paramters found via CV
-final_wf <- workflow |>
-  finalize_workflow(best_model)
-
-# Fit the best model on the training data and evaluate on the test data
-final_fit <- final_wf %>%
-  last_fit(data_split)
-
-collect_metrics(tune_results3) |>
-  filter(.metric == "accuracy") |>
-  arrange(desc(mean))
-
-
-####################################################################
-###### Allergen classification - all data #######
+################## Allergen classification - all data ##############
 ####################################################################
 
 # Cross-validatoin results
@@ -84,6 +36,31 @@ collect_metrics(tune_results_allergen) |>
 # Read in overall stat reaults
 overall_stats_allergen <- read_csv(here(
   "results/rf_overall_stats_Allergen.csv"
+))
+View(overall_stats_allergen)
+
+# Read in by class reaults
+metrics_by_class_allergen <- read_csv(here(
+  "results/rf_metric_by_target_Allergen.csv"
+))
+View(metrics_by_class_allergen)
+
+
+####################################################################
+############### Autoimmune classification - all data ###############
+####################################################################
+
+# Cross-validatoin results
+tune_results_autoimmune <- read_rds(
+  "results/tuning_results_Autoimm.rds"
+)
+collect_metrics(tune_results_autoimmune) |>
+  filter(.metric == "accuracy") |>
+  arrange(desc(mean))
+
+# Read in overall stat reaults
+overall_stats_allergen <- read_csv(here(
+  "results/rf_overall_stats_Autoimm.csv"
 ))
 View(overall_stats_allergen)
 
