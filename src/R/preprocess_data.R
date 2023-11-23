c788eae1f9b13a8ab528390172ed9eb72f66a219
mikkelnrasmussen <s193518@student.dtu.dk>
2023-11-23 23:29:47 +0100

    fix: convert allergen categories to title case

--- src/R/preprocess_data.R
+++ src/R/preprocess_data.R
@@ -71,6 +71,16 @@ df_class_label <- df_class_label |>
     )
   )
 
+# Transform the Allergen categories to title case
+df_class_label <- df_class_label |>
+  mutate(
+    category = if_else(
+      class == "Allergen",
+      str_to_title(category),
+      category
+    )
+  )
+
 # Rename "Other" categories
 df_class_label <- df_class_label |>
   mutate(
